7f032226ff7720815e836409435acb3d51a736cd
longyuhuan <2630819701@qq.com>
2022-09-28 10:06:04 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
+import 'normalize.css' // 样式标准
+import '@/assets/css/base.css' // 基本样式
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import axios from 'axios'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+
+axios.defaults.baseURL = 'http://localhost:3000' // 基本路径
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axios
 
 new Vue({
   router,
